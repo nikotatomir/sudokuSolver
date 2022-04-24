e9d8489e65c02d8b75abd1ee6c7c5bedf895432c
nikotatomir <niko.tatomir@gmail.com>
2022-04-24 12:44:14 +0200

    code for backtrackingAlgorithm class cleaned.

--- backtrackingAlgorithm.py
+++ backtrackingAlgorithm.py
@@ -7,7 +7,7 @@ class backtrackingAlgorithm:
 
 		self.rowIndexList = self.getRowIndexList()
 		self.columnIndexList = self.getColumnIndexList()
-		#self.subgridIndexList = self.getSubgridIndexList()
+		self.subgridIndexList = self.getSubgridIndexList()
 		self.subgridMatrixIndexList = self.getSubgridMatrixIndexList()
 		self.candidatesList = self.getCandidatesList()
 
@@ -25,17 +25,17 @@ class backtrackingAlgorithm:
 		columnIndexList = tuple(columnIndexList)
 		return columnIndexList
 
-	# def getSubgridIndexList(self) -> tuple:
-	# 	subgridIndexList = []
-	# 	for boxId in range(self.board.boxSize):
-	# 		subgridIndexList.append(self.board.getSubgridIndex(boxId))
-	# 	subgridIndexList = tuple(subgridIndexList)
-	# 	return subgridIndexList
+	def getSubgridIndexList(self) -> tuple:
+		subgridIndexList = []
+		for boxId in range(self.board.boxSize):
+			subgridIndexList.append(self.board.getSubgridIndex(boxId))
+		subgridIndexList = tuple(subgridIndexList)
+		return subgridIndexList
 
 	def getSubgridMatrixIndexList(self) -> tuple:
 		subgridMatrixIndexList = []
 		for boxId in range(self.board.boxSize):
-			subgridIndex = self.board.getSubgridIndex(boxId)
+			subgridIndex = self.subgridIndexList[boxId]
 			subgridMatrixIndexList.append(self.board.getSubgridMatrixIndex(subgridIndex))
 		subgridMatrixIndexList = tuple(subgridMatrixIndexList)
 		return subgridMatrixIndexList
@@ -63,15 +63,19 @@ class backtrackingAlgorithm:
 		else:
 			return False
 
-	def isCandidatesListEmpty(self, boxId):
+	def isCandidatesListEmpty(self, boxId: int) -> bool:
 		return not bool(self.candidatesList[boxId])
 
-	def backtrack(self, boxId):
-		# rowIndex, columnIndex = self.rowIndexList[boxId], self.columnIndexList[boxId]
-		# self.board.grid[rowIndex, columnIndex] = 0
+	def refillCandidatesListIfEmpty(self, boxId: int):
+		if self.isCandidatesListEmpty(boxId):
+			for candidate in range(1, self.board.gridSize+1):
+				self.candidatesList[boxId].append(candidate)
+		else:
+			pass
+
+	def backtrack(self, boxId: int) -> int:
 		boxId -= 1
 		while boxId > 0:
-			print('BACKTRACK', self.board.grid)
 			if self.board.fixedBoxValues[boxId]:
 				boxId -= 1
 			elif self.isCandidatesListEmpty(boxId):
@@ -80,53 +84,30 @@ class backtrackingAlgorithm:
 				boxId -= 1
 			else:
 				break
-			
-		
 		return boxId
-
-
-
 
 
 	def solve(self):
 		currentBoxId = 0
-		while currentBoxId < 81:
-			print('currentBoxId ->', currentBoxId)
+		while currentBoxId < self.board.boxSize:
 			if self.board.fixedBoxValues[currentBoxId] == 0:
-				
-				if self.isCandidatesListEmpty(currentBoxId):
-					for candidate in range(1, self.board.gridSize+1):
-						self.candidatesList[currentBoxId].append(candidate)
-				else:
-					pass
-
+				self.refillCandidatesListIfEmpty(currentBoxId)		
 				while self.candidatesList[currentBoxId]:
 					currentCandidate, rowIndex, columnIndex = self.candidatesList[currentBoxId][0], self.rowIndexList[currentBoxId], self.columnIndexList[currentBoxId]
-					print ('CURRENT CANDIDATE ->', currentCandidate)
 					if self.validCandidate(currentCandidate, currentBoxId):
 						self.board.grid[rowIndex, columnIndex] = currentCandidate
-						print(self.board.grid)
 						self.candidatesList[currentBoxId].remove(currentCandidate)
 						nextBoxId = currentBoxId + 1
-						# sys.exit('EXIT')
 						break
-
 					else:
 						self.candidatesList[currentBoxId].remove(currentCandidate)
 						if self.isCandidatesListEmpty(currentBoxId):
-							print('note', self.candidatesList[currentBoxId])
 							rowIndex, columnIndex = self.rowIndexList[currentBoxId], self.columnIndexList[currentBoxId]
 							self.board.grid[rowIndex, columnIndex] = 0
 							nextBoxId = self.backtrack(currentBoxId)
-
-							# nextBoxId = currentBoxId - 1
-							# while self.board.fixedBoxValues[nextBoxId] != 0 or not self.candidatesList[nextBoxId]:
-							# 	nextBoxId -= 1
 						else:
 							pass
-				
 				currentBoxId = nextBoxId
-			
 			else: 
 				currentBoxId += 1
 			
