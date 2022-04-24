50a3a9ab8248ed8e0d4dd7cdc49f5b1249f9c97c
nikotatomir <niko.tatomir@gmail.com>
2022-04-24 21:12:00 +0200

    the main run.py file is commented and reorganized.

--- run.py
+++ run.py
@@ -4,15 +4,33 @@ from board import board
 from backtrackingAlgorithm import backtrackingAlgorithm
 from visualization import visualization
 
-board = board('4x4.txt') 
+#-----------------------USER-INPUTS-----------------------#
+fileName = '4x4.txt'
+
+#-------------------------SOLVER--------------------------#
+# Instanciating board class. Loading sudoku puzzle and computing all necessary properties for computation
+print('Reading Sudoku Puzzle...')
+board = board('9x9.txt') 
+
+# Instanciating visualization class.
 visualize = visualization(board)
+print('Plotting Initial Sudoku Puzzle...')
 visualize.plotUnsolvedSudoku()
+
+# Instanciating the backtrackingAlgorithm class and computing the necessary properties for further computation.
 sudoku = backtrackingAlgorithm(board)
-print('Solving...')
+# Running the backtracking algorithm
+print('Solving...', end=' ')
 sudoku.solve()
 print('Finished!')
+
+# Plotting the solved Sudoku Puzzle
 visualize.plotSolvedSudoku()
+
+# Checking if all contraints are met
+print('Checking If All Game Contraints Are Met...', end=' ')
 if board.validBoard():
-	print('Solution Valid!')
+	print('Yes, Solution Valid!')
 else:
-	print('Solution Not Valid!')
+	print('No, Solution Not Valid!')
+print('Done.')
